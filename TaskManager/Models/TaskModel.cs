e31e3c335cc460aa7f30cdb78ee47b0d09bbe18d
Andrzej Kołacz <Jendrula95@hotmail.com>
2023-01-02 19:11:21 +0100

    Dodaj pliki projektów.

---
+++ TaskManager/Models/TaskModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TaskManager.Models;
+
+
+namespace TaskManager.Models
+{
+    public class TaskModel
+    {
+        public int TaskId { get; set; }
+        [DisplayName("Nazwa")]
+        public string Name { get; set; }
+        [DisplayName("Opis")]
+        public string  Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
